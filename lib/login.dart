c2b9ea223b6754f054d03bc7c784036175a3275a
Howard <haoaaa2014@gmail.com>
2025-04-20 19:38:25 +0800

    Updated 20/4 - Login Page UI

--- lib/login.dart
+++ lib/login.dart
@@ -8,63 +8,124 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
-  String _appLogo = 'assets/images/appLogo.png';
+  final String _appLogo = 'assets/images/appLogo.png';
 
   final TextEditingController _emailCTRL = TextEditingController();
   final TextEditingController _passCTRL = TextEditingController();
 
-
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text('Login'),
-        backgroundColor: Colors.white,
-      ),
+      appBar: AppBar(title: Text('Login'), backgroundColor: Colors.white),
       body: Center(
-        child: Padding(
+        child: Container(
+          width: 390,
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
+            children: <Widget>[
               Stack(
                 fit: StackFit.loose,
                 alignment: AlignmentDirectional.center,
                 children: [
+                  SizedBox(height: 400),
                   Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1
-                      )
-                    ),
-                    child: Image.asset(
-                        _appLogo
-                    ),
+                    width: 300,
+                    height: 300,
+                    child: Image.asset(_appLogo, fit: BoxFit.contain),
                   ),
                 ],
               ),
               const Text(
-                'Welcome!'
+                'Welcome',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
+              SizedBox(height: 20),
+              Container(),
               TextField(
                 controller: _emailCTRL,
                 keyboardType: TextInputType.emailAddress,
+                obscureText: true,
                 decoration: InputDecoration(
-                  labelText: 'Enter your email',
+                  hintText: 'Email',
+                  filled: true,
+                  fillColor: Colors.black12,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
+              const SizedBox(height: 10),
               TextField(
                 controller: _passCTRL,
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
-                  labelText: 'Enter your password'
+                  filled: true,
+                  fillColor: Colors.black12,
+                  hintText: 'Password',
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
-              )
+              ),
+              const SizedBox(height: 10),
+              Align(
+                //Forgot Password Link Button
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                  child: Text(
+                    'Forgot password?',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                height: 43,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextButton(onPressed: () {},
+                  child: Text(
+                      'New user?Sign Up',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                  )
+              ),
             ],
           ),
         ),
@@ -72,5 +133,3 @@ class _LoginState extends State<Login> {
     );
   }
 }
-
-
